8ee9ca38e62c01c4d7add192feec298bb2c1e20c
chaar99 <carmenlopezcalvo.4c@gmail.com>
2020-11-20 13:01:49 +0100

    arreglo de inicio de sesion

--- inicioSesion_usuario.php
+++ inicioSesion_usuario.php
@@ -4,33 +4,36 @@
     $myObj = json_decode(file_get_contents('php://input'), true);
     $correo = $myObj['correo'];
     $contra = $myObj['password'];
+    // $correo = "carmenlopezcalvo.4c@gmail.com";
+    // $contra = 123;
     $obj =  conectaBD::singleton();
 
     $result = $obj->inicioSesion($correo);
-   
+        
     //compruebo si exixste el correo
     if( count($result) == 1){
 
         // si existe el correo compuebo la constraseña
-        if(Password::verify($contra,$result)){
+        if(Password::verify($contra,$result[0])){
            
-          // session_start();
-          // $_SESSION["correo"] = $correo;
-
-            $salida = $correo;
+            // session_start();
+            // $_SESSION["correo"] = $correo;
+            $result2 = $obj->inicioSesion2($correo);
+            
+            //$salida -> $correo;
             
         }else{
 
-            $salida = 0;
+            $result = 0;
         }
 
     }else{
 
-        $salida = 1;
+        $result = 1;
 
     }
     
-    echo json_encode($salida);
+    echo json_encode($result2);
        
        
 ?>
