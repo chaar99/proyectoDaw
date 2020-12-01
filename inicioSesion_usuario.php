7157ca3196c93625eb95ff965fd98a66b67cd854
chaar99 <carmenlopezcalvo.4c@gmail.com>
2020-12-01 16:40:36 +0100

    arreglo inicio de sesión

--- inicioSesion_usuario.php
+++ inicioSesion_usuario.php
@@ -1,40 +1,31 @@
 <?php 
     include('conectaBDconPDO.php');
     include('Password.php');
+    
     $myObj = json_decode(file_get_contents('php://input'), true);
     $correo = $myObj['correo'];
     $contra = $myObj['password'];
     // $correo = "carmenlopezcalvo.4c@gmail.com";
-    // $contra = 123;
+    // $contra = 1223;
     $obj =  conectaBD::singleton();
-
     $result = $obj->inicioSesion($correo);
-    
-    //compruebo si exixste el correo
-    if( count($result) == 1){
 
+    //compruebo si exixste el correo
+    if($result == ""){
+
+        $salida = "El correo no existe";
+
+    }else{
         // si existe el correo compuebo la constraseña
         if(Password::verify($contra,$result[0])){
             
             // session_start();
             // $_SESSION["correo"] = $correo;
-            
             $salida =$obj->inicioSesion2($correo);
-            
-            
         }else{
-
-            $salida = "La contraseña no es correcta";
-            
+           $salida = "la contraseña es incorrecta";
         }
-
-    }else{
-
-        $salida = "El correo no existe";
-        
     }
-    
-    echo json_encode($salida);
-       
-       
+
+    echo json_encode($salida, JSON_UNESCAPED_UNICODE);
 ?>
